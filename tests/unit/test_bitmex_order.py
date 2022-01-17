1e22b6e6228b722548156a30bb8b3bf0ea94804e
Niclas Hummel <31075753+dignitas123@users.noreply.github.com>
2022-01-17 12:49:04 +0700

    added mockFolder structure and position size test

--- tests/unit/test_bitmex_order.py
+++ tests/unit/test_bitmex_order.py
@@ -1,17 +1,44 @@
 import unittest
+from unittest.mock import PropertyMock, patch
+import os
 
-from algo_trader.clients.bitmex import BitmexClient
+from algo_trader.clients.bitmex import BitmexClient, BitmexOrder
 
 from unittest.mock import MagicMock
+from tests.unit.mock.mockBrokerSettings import mockSettings
+mockSettingsPath = os.path.dirname(
+    os.path.abspath(__file__))  # current directory
 
 
 class BitmexOrderTestCase(unittest.TestCase):
     """Test BitmexOrder Class Functions with Client Mock Data"""
 
     def setUp(self):
-        self.mockClient = BitmexClient(api_key='', api_secret='')
-        self.mockClient.get_histories = MagicMock(return_value=3)
-        self.mockClient.get_histories(3, 4, 5, key='value')
+        self.mockClient = BitmexClient()
+        type(self.mockClient).acc_balance = PropertyMock(
+            return_value=100000000)
+        self.mockOrder = BitmexOrder(
+            ['ETHUSD', 'XBTUSD'],
+            self.mockClient,
+            mockSettings,
+            mockSettingsPath
+        )
 
-    def test_get_histories(self):
-        self.mockClient.get_histories.assert_called_with(3, 4, 5, key='value')
+    def test_calc_pos_size(self):
+        self.mockClient.client.last_current_price = MagicMock(
+            return_value=50000)
+        self.mockOrder.settings.symbols = {'XBTUSD': {'position_size': 1.0}}
+        pos_size = self.mockOrder.calc_pos_size('XBTUSD', 200)
+        self.assertEqual(pos_size, 91700)
+        self.mockOrder.settings.symbols = {'XBTUSD': {'position_size': 0.5}}
+        pos_size = self.mockOrder.calc_pos_size('XBTUSD', 200)
+        self.assertEqual(pos_size, 45900)
+
+        self.mockClient.client.last_current_price = MagicMock(
+            return_value=3000)
+        self.mockOrder.settings.symbols = {'ETHUSD': {'position_size': 1.0}}
+        pos_size = self.mockOrder.calc_pos_size('ETHUSD', 50)
+        self.assertEqual(pos_size, 200)
+        self.mockOrder.settings.symbols = {'ETHUSD': {'position_size': 0.5}}
+        pos_size = self.mockOrder.calc_pos_size('ETHUSD', 50)
+        self.assertEqual(pos_size, 100)
